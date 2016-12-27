96df3711078f7ddb054e8c706e3d665b29ad832c
Antonis Kalipetis <akalipetis@gmail.com>
2016-12-27 18:13:55 +0200

    Upgrade libraries and Node version
    Fix anonymous functions to use the new syntax and any changes needed by the BotBuilder library.
    
    Also, include a .jshintrc file.

--- app.js
+++ app.js
@@ -1,64 +1,71 @@
+'use strict';
+
 require('dotenv').config();
-var builder = require('botbuilder');
-var jade = require('jade');
-var restify = require('restify');
+const builder = require('botbuilder');
+const jade = require('jade');
+const restify = require('restify');
 
-var server = restify.createServer();
 
-var iframeUrl = 'https://webchat.botframework.com/embed/' + process.env.APP_ID + '?s=' +
-    process.env.APP_IFRAME_SECRET;
+// Create the API server for the bot
+let server = restify.createServer();
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Your bot is listening to https://${process.env.SL_PUBLIC_URL}`);
+    console.log(`Make sure you have added https://${process.env.SL_PUBLIC_URL}/api/messages as your messaging endpoint`);
+});
 
-var helloBot = new builder.BotConnectorBot();
-helloBot.add('/', new builder.CommandDialog()
-    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
-    .matches('^quit', builder.DialogAction.endDialog())
-    .matches('^what is the best IDE', builder.DialogAction.beginDialog('/sourcelair'))
-    .onDefault(function (session) {
+// Setup the integration with BotFramework
+let connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PASSWORD,
+});
+server.post('/api/messages', connector.listen());
+
+// Create a bot and add dialogs
+let helloBot = new builder.UniversalBot(connector);
+helloBot.dialog('/', new builder.IntentDialog()
+    .matches(/^set name/i, builder.DialogAction.beginDialog('/profile'))
+    .matches(/^quit/i, builder.DialogAction.endDialog())
+    .matches(/^what.+(best|favorite).+IDE/i, builder.DialogAction.beginDialog('/sourcelair'))
+    .matches(/^(\?|help)/i, builder.DialogAction.beginDialog('/help'))
+    .onDefault((session) => {
         if (!session.userData.name) {
             session.beginDialog('/profile');
         } else {
-            session.send('Hm, I did not understand you %s :(', session.userData.name);
+            session.send(`Hm, I did not understand you ${session.userData.name} :(`);
         }
     }));
-helloBot.add('/profile',  [
-    function(session) {
+helloBot.dialog('/profile',  [
+    (session) => {
         if (session.userData.name) {
             builder.Prompts.text(session, 'What would you like to change it to?');
         } else {
             builder.Prompts.text(session, 'Hi! What is your name?');
         }
     },
-    function(session, results) {
+    (session, results) => {
         session.userData.name = results.response;
-        session.send('Hello %s, it\'s great to meet you. I\'m bot.', session.userData.name);
+        session.send(`Hello ${session.userData.name}, it\'s great to meet you. I\'m bot.`);
         session.endDialog();
     }
 ]);
-helloBot.add('/sourcelair',  [
-    function(session) {
+helloBot.dialog('/sourcelair',  [
+    (session) => {
         session.send('It\'s SourceLair of course <3');
         session.endDialog();
     }
 ]);
-helloBot.add('/help', [
-    function(session) {
+helloBot.dialog('/help', [
+    (session) => {
         session.send('You can always ask me some questions, for example what is my favorite IDE');
         session.endDialog();
     }
 ]);
 
-server.get('/', function indexHTML(req, res, next) {
+// Setup the iframe for the webchat
+let iframeUrl = `https://webchat.botframework.com/embed/${process.env.BOT_HANDLE}?s=${process.env.APP_IFRAME_SECRET}`;
+server.get('/', (req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.writeHead(200);
-    res.end(
-        jade.renderFile('index.jade', {iframeUrl: iframeUrl}));
+    res.end(jade.renderFile('index.jade', {iframeUrl: iframeUrl}));
     next();
-});
-
-server.use(helloBot.verifyBotFramework(
-    {appId: process.env.APP_ID, appSecret: process.env.APP_SECRET}));
-server.post('/v1/messages', helloBot.listen());
-
-server.listen(process.env.PORT || 3000, function () {
-    console.log('%s listening to %s', server.name, server.url);
 });
